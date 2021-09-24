0cb01ef09caf0b173cf2695c2200608957f9dd89
taemin <87377069+gotokakao@users.noreply.github.com>
2021-09-24 17:57:07 +0900

    s3 upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -66,8 +66,8 @@ export const postUpload = async (req, res) => {
         const newVideo = await Video.create({
             title : title,
             owner : _id, 
-            fileUrl : video[0].path,
-            thumbUrl : thumb[0].path,
+            fileUrl : video[0].location,
+            thumbUrl : thumb[0].location,
             description : description,
             hashtags : Video.formatHashtags(hashtags),
         });
